c5c48ec6c5dc83d798e40952f34cef4988a9d79e
Jay-Chow <jayashish.chowdhury@gmail.com>
2020-07-16 11:57:15 +0530

    First Program

---
+++ Hello-World.java
@@ -0,0 +1,18 @@
+class MainCls
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World");
+		
+	}
+
+
+}
+
+//makes .class file for every class created
+//Can have more than 1 main class and to execute we can run any of the classes containing main 
+
+
+
+//to do
+//check if multiple mains work under the same class or not?
